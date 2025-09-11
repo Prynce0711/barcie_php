f4811ed63ec2652545f2afb014c861ddc95c0204
Prynce0711 <pc.clemente11@gmail.com>
2025-09-11 12:16:28 +0800

    new buttons but still no contents

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('click', e => {
-  if (e.target.matches('.btn-ajax')) {
-    e.preventDefault();
-    fetch(e.target.href, { credentials: 'same-origin' })
-      .then(r => r.text()).then(t => alert('Action complete'))
-  }
-})
