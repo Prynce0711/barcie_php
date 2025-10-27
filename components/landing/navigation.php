277095b1ed075b97421f9657b8a78daff21384cd
pryncecarloclemente-byte <pryncecarlo.clemente@email.lcup.edu.ph>
2025-10-27 22:46:08 +0800

    Reorganize navigation items and remove redundant line in index.php

--- components/landing/navigation.php
+++ components/landing/navigation.php
@@ -25,15 +25,17 @@
         <li class="nav-item">
           <a class="nav-link" href="#services">Services</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#contact">Contact</a>
-        </li>
+
 
         <li class="nav-item">
           <a class="nav-link" href="#caterings">Caterings</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#event-stylists">Event Stylists</a>
+        </li>
+
+                <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
         </li>
       </ul>
 
