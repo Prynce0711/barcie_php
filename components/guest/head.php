fe89d60ec1f8e4080219f2f14451d2f6acff4328
pryncecarlo.clemente-byte <pc.clemente11@gmail.com>
2025-11-08 22:30:23 +0800

    chore: Set default timezone to Philippine Time in various files for consistency

--- components/guest/head.php
+++ components/guest/head.php
@@ -1,3 +1,4 @@
+  <?php date_default_timezone_set('Asia/Manila'); ?>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="user-id" content="<?php echo $user_id; ?>">
